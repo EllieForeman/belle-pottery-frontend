c72d6a2138686eac35700284106bf959d511810e
EllieForeman <foreman.eleanor@gmail.com>
2025-11-30 18:40:19 +0000

    testing checkout session expire 60 seconds

--- app/api/checkout_sessions/route.ts
+++ app/api/checkout_sessions/route.ts
@@ -68,30 +68,24 @@ export async function POST(req: Request) {
     };
 
     const shipping_options: ShippingOption[] = isOnlyBrooch ? [freeBroochOption] : [standardOption];
-    const expiresAt = Math.floor(Date.now() / 1000) + 60;
+    const expiresAt = Math.floor(Date.now() / 1000) + 10 * 60;
 
     const session = await stripe.checkout.sessions.create({
-    mode: "payment",
-    payment_method_types: ["card"],
-    line_items,
-    billing_address_collection: "required",
-    shipping_address_collection: { allowed_countries: ["GB"] },
-    shipping_options,
-    phone_number_collection: { enabled: true },
-    success_url: `${process.env.NEXT_PUBLIC_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${process.env.NEXT_PUBLIC_URL}/cart`,
-    expires_at: expiresAt,
-    metadata: {
-      cart: JSON.stringify(
-        cart.map((item: any) => ({
-          id: item.id,
-          quantity: 1,
-        }))
-      ),
-    },
-  });
+      mode: "payment",
+      payment_method_types: ["card"],
+      line_items,
+        billing_address_collection: "required",
+      shipping_address_collection: {
+        allowed_countries: ["GB"], 
+      },
+      shipping_options,
+      phone_number_collection: { enabled: true },
+      success_url: `${process.env.NEXT_PUBLIC_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.NEXT_PUBLIC_URL}/cart`,
+      expires_at: expiresAt, 
+    });
 
-  return NextResponse.json({ url: session.url });
+    return NextResponse.json({ url: session.url });
   } catch (err) {
     console.error("Checkout session error:", err);
     return NextResponse.json(
