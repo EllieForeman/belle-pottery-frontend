1e1fcfb292ff752f1d649586c1ca0881cbbbf1c9
EllieForeman <foreman.eleanor@gmail.com>
2025-12-09 21:29:56 +0000

    removed cap of 25 items

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,13 +1,19 @@
 const BASE_URL = process.env.NEXT_PUBLIC_CMS_BASE_URL;
 
 export async function fetchFromCMS(endpoint: string, query = "") {
-  const url = `${BASE_URL}/${endpoint}?populate=*${query ? `&${query}` : ""}`;
+  const baseQuery = "populate=*&pagination[pageSize]=200";
+
+  const url = `${BASE_URL}/${endpoint}?${baseQuery}${
+    query ? `&${query}` : ""
+  }`;
+
   try {
     const res = await fetch(url, {});
 
     if (!res.ok) {
       throw new Error(`Failed to fetch: ${res.status} ${res.statusText}`);
     }
+
     return await res.json();
   } catch (error) {
     console.error("Error fetching data:", error);
