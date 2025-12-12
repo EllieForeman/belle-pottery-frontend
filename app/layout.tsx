f10396e045e01937bdc06378fd125704741d6cf8
EllieForeman <foreman.eleanor@gmail.com>
2025-12-12 20:53:06 +0000

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,25 @@ const sourceSans = Source_Sans_3({
 
 export const metadata: Metadata = {
   title: "Isabelle Proffitt Pottery",
-  description:
-    "Isabelle Proffitt makes wood-fired, salt-glazed ceramics driven by process, material curiosity, and playful making. Shop included bird pins, buttons, mugs, jars, mostly wood-fired, salt-glazed ceramics by Isabelle Proffitt.",
+  description: "Isabelle Proffitt makes wood-fired, salt-glazed ceramics driven by process, material curiosity, and playful making.",
+  openGraph: {
+    title: "Isabelle Proffitt Pottery",
+    description: "Isabelle Proffitt makes wood-fired, salt-glazed ceramics driven by process, material curiosity, and playful making.",
+    images: [
+      {
+        url: "../public/headshot1.png",
+        width: 1200,
+        height: 630,
+        alt: "Headshot of Belle"
+      }
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Isabelle Proffitt makes wood-fired, salt-glazed ceramics driven by process, material curiosity, and playful making.",
+    description: "Isabelle's website includes ",
+    images: ["../public/headshot1.png"]
+  }
 };
 
 export default function RootLayout({
